f471ea446ab69d600b7a2b095016a3f6ca51c2ae
Jacinto Acosta <jacintoaczz@gmail.com>
2021-07-08 12:32:53 -0400

    - Cambios hechos en la clase del 08/07/2021

--- carritotucamper.js
+++ carritotucamper.js
@@ -10,7 +10,6 @@ const shoppingCartItemsContainer = document.querySelector(
 
 function comprarBoton(event) {
   let itemsDelCarrito = JSON.parse(localStorage.getItem("itemsDelCarrito"));
-  let existeElemento = false;
 
   const boton = event.target;
   const item = boton.closest(".item");
@@ -30,96 +29,109 @@ function comprarBoton(event) {
   };
 
   // Haciendo la misma comprobacion, pero con .filter()
-  // let nuevoArreglo, nuevaCantidad, otrosArticulos, alLocalStorage;
-  // if (itemsDelCarrito) {
-  //   nuevoArreglo = itemsDelCarrito.filter(
-  //     (item) => item.id === itemDelCarrito.id
-  //   );
+  let nuevoArreglo, otrosArticulos, alLocalStorage;
+  if (itemsDelCarrito !== null) {
+    // El metodo filter() nos retorna un nuevo arreglo con elementos que coincidan
+    // con la condicion especificada.
+    // Las funciones flecha, siempre que tengan mas de una linea deben tener
+    // un 'return' especificado, si no, no devolveran nada.
+    nuevoArreglo = itemsDelCarrito.filter(
+      (item) => item.id === itemDelCarrito.id
+    );
 
-  //   if (nuevoArreglo[0]) {
-  //     nuevoArreglo[0].cantidad = nuevoArreglo[0].cantidad + 1;
-  //   }
+    otrosArticulos = itemsDelCarrito.filter(
+      (item) => item.id !== itemDelCarrito.id
+    );
 
-  //   otrosArticulos = itemsDelCarrito.filter(
-  //     (item) => item.id !== itemDelCarrito.id
-  //   );
+    if (nuevoArreglo[0]) {
+      nuevoArreglo[0].cantidad = nuevoArreglo[0].cantidad + 1;
 
-  //   // let arreglo = ["Jacinto", "Acosta", "Gonzalez"]; arreglo.length (resultado = 3)
-  //   // ...arreglo = "Jacinto", "Acosta", "Gonzalez";
-  //   alLocalStorage = [...nuevoArreglo, ...otrosArticulos];
+      // let arreglo = ["Jacinto", "Acosta", "Gonzalez"]; arreglo.length (resultado = 3)
+      // ...arreglo = "Jacinto", "Acosta", "Gonzalez";
+      alLocalStorage = [...nuevoArreglo, ...otrosArticulos];
+    } else {
+      alLocalStorage = [itemDelCarrito, ...otrosArticulos];
+    }
 
-  //   console.log("Arreglo que vamos a guardar en el localStorage");
-  //   console.log(alLocalStorage);
-  // }
+    // Solo se pueden escribir strings en el local storage, no objetos u arreglos
+    // debido a esto, se usa el metodo stringify() para poder escribir dichos tipos
+    // de datos (u objetos).
+    localStorage.setItem("itemsDelCarrito", JSON.stringify(alLocalStorage));
+  } else {
+    alLocalStorage = [itemDelCarrito];
+    localStorage.setItem("itemsDelCarrito", JSON.stringify(alLocalStorage));
+  }
 
+  calculateCartItems();
   // Compruebo si el articulo se guardo previamente, y de ser asi, actualizo la
   // la cantidad de articulos.
-  if (itemsDelCarrito) {
-    itemsDelCarrito.forEach((item) => {
-      if (item.id === itemDelCarrito.id) {
-        existeElemento = true;
+  // if (itemsDelCarrito) {
+  //   itemsDelCarrito.forEach((item) => {
+  //     if (item.id === itemDelCarrito.id) {
+  //       existeElemento = true;
 
-        // Actualizamos la cantidad de articulos en funcion a los encontrados en el
-        // array.
-        let cantidadAnterior = parseInt(item.cantidad);
-        let nuevaCantidad = cantidadAnterior + 1;
-        item.cantidad = nuevaCantidad;
-      }
-    });
-  }
+  //       // Actualizamos la cantidad de articulos en funcion a los encontrados en el
+  //       // array.
+  //       let cantidadAnterior = parseInt(item.cantidad);
+  //       let nuevaCantidad = cantidadAnterior + 1;
+  //       item.cantidad = nuevaCantidad;
+  //     }
+  //   });
+  // }
 
-  if (!existeElemento) {
-    if (itemsDelCarrito === null) {
-      itemsDelCarrito = [];
-      itemsDelCarrito.push(itemDelCarrito);
-    } else {
-      itemsDelCarrito.push(itemDelCarrito);
-    }
-  }
-  console.log("Items almacenados hasta ahora:");
-  console.log(itemsDelCarrito);
+  // if (!existeElemento) {
+  //   if (itemsDelCarrito === null) {
+  //     itemsDelCarrito = [];
+  //     itemsDelCarrito.push(itemDelCarrito);
+  //   } else {
+  //     itemsDelCarrito.push(itemDelCarrito);
+  //   }
+  // }
+  // console.log("Items almacenados hasta ahora:");
+  // console.log(itemsDelCarrito);
 
-  // Usamos el metodo JSON.stringify() para convertir todos los valores del arreglo
-  // en strings.
-  localStorage.setItem("itemsDelCarrito", JSON.stringify(itemsDelCarrito));
+  // // Usamos el metodo JSON.stringify() para convertir todos los valores del arreglo
+  // // en strings.
+  // localStorage.setItem("itemsDelCarrito", JSON.stringify(itemsDelCarrito));
 
-  itemTienda(itemTitulo, itemPrecio, itemImg);
+  // itemTienda(itemTitulo, itemPrecio, itemImg);
 }
 
-function itemTienda(itemTitulo, itemPrecio, itemImg) {
-  const shoppingCartRow = document.createElement("div");
-  const shoppingCartContent = `
- <div class="row shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImg} width=50 class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitulo}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrecio}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
-  shoppingCartRow.innerHTML = shoppingCartContent;
-  shoppingCartItemsContainer.append(shoppingCartRow);
-}
+// function itemTienda(itemTitulo, itemPrecio, itemImg) {
+//   const shoppingCartRow = document.createElement("div");
+//   const shoppingCartContent = `
+//  <div class="row shoppingCartItem">
+//         <div class="col-6">
+//             <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+//                 <img src=${itemImg} width=50 class="shopping-cart-image">
+//                 <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitulo}</h6>
+//             </div>
+//         </div>
+//         <div class="col-2">
+//             <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+//                 <p class="item-price mb-0 shoppingCartItemPrice">${itemPrecio}</p>
+//             </div>
+//         </div>
+//         <div class="col-4">
+//             <div
+//                 class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+//                 <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+//                     value="1">
+//                 <button class="btn btn-danger buttonDelete" type="button">X</button>
+//             </div>
+//         </div>
+//     </div>`;
+//   shoppingCartRow.innerHTML = shoppingCartContent;
+//   shoppingCartItemsContainer.append(shoppingCartRow);
+// }
 
 /**  Carga taoda la informacion del localStorage en variables para manipularla luego.*/
 const prepareCartData = () => {
   const cartData = JSON.parse(localStorage.getItem("itemsDelCarrito"));
   const formulario = document.querySelector(".formulario");
 
-  if (cartData) {
+  if (cartData !== null && cartData !== []) {
+    // Un forEach no retorna nada.
     cartData.forEach((cartItem) => {
       const itemDiv = createCartItem(cartItem);
       formulario.append(itemDiv);
@@ -139,45 +151,62 @@ const createCartItem = (data) => {
   let wrapperDiv = document.createElement("div");
   wrapperDiv.setAttribute("class", "cart-item");
 
+  //  COmo ya sabemos la estructura que va a tener el elemento HTML, lo construimos
+  //  de esta manera para que sea mas corto.
+  const cartItemHTML = `
+  <div class="articulos">
+    <img src="${data.imagen}" alt="${data.nombre}" class="product-img">
+  </div>
+    <div class="articulos">
+      <p>${data.nombre}</p>
+    </div>
+  <div class="articulos">
+    <p>${data.cantidad}</p>
+  </div>
+  <div class="articulos">
+    <p>${data.precio}</p>
+  </div>
+`;
+
   // Creamos el div que contiene la imagen del producto
-  let imageDiv = document.createElement("div");
-  imageDiv.setAttribute("class", "articulos");
-  let productImage = document.createElement("img");
-  productImage.setAttribute("src", `${data.imagen}`);
-  productImage.setAttribute("alt", `${data.nombre}`);
-  productImage.setAttribute("class", "product-img");
+  // let imageDiv = document.createElement("div");
+  // imageDiv.setAttribute("class", "articulos");
+  // let productImage = document.createElement("img");
+  // productImage.setAttribute("src", `${data.imagen}`);
+  // productImage.setAttribute("alt", `${data.nombre}`);
+  // productImage.setAttribute("class", "product-img");
 
-  imageDiv.append(productImage);
+  // imageDiv.append(productImage);
 
-  // Creamos el div que contiene el nombre del producto
-  let articleDiv = document.createElement("div");
-  articleDiv.setAttribute("class", "articulos");
-  let articleP = document.createElement("p");
+  // // Creamos el div que contiene el nombre del producto
+  // let articleDiv = document.createElement("div");
+  // articleDiv.setAttribute("class", "articulos");
+  // let articleP = document.createElement("p");
 
-  articleP.innerHTML = `${data.nombre}`;
-  articleDiv.append(articleP);
+  // articleP.innerHTML = `${data.nombre}`;
+  // articleDiv.append(articleP);
 
-  // Creamos el div que contiene la cantidad del producto
-  let amountDiv = document.createElement("div");
-  amountDiv.setAttribute("class", "articulos");
-  let amountP = document.createElement("p");
+  // // Creamos el div que contiene la cantidad del producto
+  // let amountDiv = document.createElement("div");
+  // amountDiv.setAttribute("class", "articulos");
+  // let amountP = document.createElement("p");
 
-  amountP.innerHTML = `${data.cantidad}`;
-  amountDiv.append(amountP);
+  // amountP.innerHTML = `${data.cantidad}`;
+  // amountDiv.append(amountP);
 
-  // Creamos el div que contiene el precio del producto
-  let priceDiv = document.createElement("div");
-  priceDiv.setAttribute("class", "articulos");
-  let priceP = document.createElement("p");
+  // // Creamos el div que contiene el precio del producto
+  // let priceDiv = document.createElement("div");
+  // priceDiv.setAttribute("class", "articulos");
+  // let priceP = document.createElement("p");
 
-  priceP.innerHTML = `${data.precio}`;
-  priceDiv.append(priceP);
+  // priceP.innerHTML = `${data.precio}`;
+  // priceDiv.append(priceP);
 
   // Creamos el boton de eliminar producto
   let deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("class", "btn-delete");
-  deleteBtn.addEventListener("click", function () {
-    deleteItem(data.id);
+  deleteBtn.addEventListener("click", function (e) {
+    deleteItem(e, data.id);
   });
 
   let iconSpan = document.createElement("span");
@@ -188,17 +217,37 @@ const createCartItem = (data) => {
   deleteBtn.append(iconSpan);
 
   // Añadimos todos los elementos creados al contenedor
-  wrapperDiv.append(imageDiv);
-  wrapperDiv.append(articleDiv);
-  wrapperDiv.append(amountDiv);
-  wrapperDiv.append(priceDiv);
+  // wrapperDiv.append(imageDiv);
+  // wrapperDiv.append(articleDiv);
+  // wrapperDiv.append(amountDiv);
+  // wrapperDiv.append(priceDiv);
+  wrapperDiv.innerHTML = cartItemHTML;
   wrapperDiv.append(deleteBtn);
 
   return wrapperDiv;
 };
 
-const deleteItem = (itemId) => {
-  console.log(itemId);
+const deleteItem = (e, itemId) => {
+  const formulario = document.querySelector(".formulario");
+  const totalAmountP = document.querySelector(".total > p");
+  // Borramos el div que tiene el boton presionado
+  e.target.parentElement.remove();
+  totalAmountP.remove();
+
+  const cartItems = JSON.parse(localStorage.getItem("itemsDelCarrito"));
+  const newCartItems = cartItems.filter((item) => item.id !== itemId);
+
+  localStorage.setItem("itemsDelCarrito", JSON.stringify(newCartItems));
+
+  if (newCartItems === []) {
+    let infoP = document.createElement("p");
+    infoP.setAttribute("class", "info-p");
+
+    infoP.innerHTML = `No tiene productos en su carrito actualmente`;
+    formulario.append(infoP);
+  }
+
+  calculateTotalAmount(newCartItems);
 };
 
 const calculateTotalAmount = (itemsData) => {
@@ -206,10 +255,31 @@ const calculateTotalAmount = (itemsData) => {
   const totalDiv = document.querySelector(".total");
   let totalP = document.createElement("p");
 
-  itemsData.forEach((item) => {
-    totalAmount = totalAmount + parseFloat(item.precio * item.cantidad);
-  });
-  totalP.innerHTML = `TOTAL: ${totalAmount.toFixed(3)}`;
+  if (itemsData !== null && itemsData !== []) {
+    itemsData.forEach((item) => {
+      totalAmount = totalAmount + parseFloat(item.precio * item.cantidad);
+    });
+    totalP.innerHTML = `TOTAL: ${totalAmount.toFixed(3)}`;
+  } else {
+    totalP.innerHTML = `TOTAL: ${totalAmount}`;
+  }
 
   totalDiv.append(totalP);
+};
+
+const calculateCartItems = () => {
+  let totalCartItems = 0;
+  const cartItems = JSON.parse(localStorage.getItem("itemsDelCarrito"));
+  const cartItemsBadge = document.querySelector(".cart-badge");
+
+  if (cartItems !== null) {
+    cartItems.forEach((item) => {
+      totalCartItems += parseInt(item.cantidad);
+    });
+
+    cartItemsBadge.classList.add("show-badge");
+    cartItemsBadge.innerHTML = totalCartItems;
+  } else {
+    cartItemsBadge.classList.remove("show-badge");
+  }
 };
